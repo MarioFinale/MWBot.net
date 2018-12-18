8a78ec6aa824bb718b48178d7289e5b7c9338c55
MarioFinale <MarioFinale@hotmail.com>
2018-12-18 15:54:28 -0300

    From PeriodiBOT

---
+++ ConfigFile.vb
@@ -0,0 +1,34 @@
+﻿Public Class ConfigFile
+    Private _path As String
+    Public ReadOnly Property Path As String
+        Get
+            Return _path
+        End Get
+    End Property
+
+    Public ReadOnly Property Params As String()
+        Get
+            If Not IO.File.Exists(_path) Then
+                IO.File.Create(_path).Close()
+            End If
+            Return IO.File.ReadAllLines(_path)
+        End Get
+    End Property
+
+    Public Sub New(ByVal tPath As String)
+        If Not IO.File.Exists(tPath) Then
+            IO.File.Create(tPath).Close()
+        End If
+        _path = tPath
+    End Sub
+
+    Public Sub New(ByVal filePath As String, params As String())
+        If IO.File.Exists(_path) Then
+            IO.File.Delete(_path)
+            IO.File.Create(_path).Close()
+            IO.File.WriteAllLines(_path, params)
+        End If
+        _path = filePath
+    End Sub
+
+End Class
---
+++ GlobalVars.vb
@@ -0,0 +1,23 @@
+﻿Option Strict On
+Option Explicit On
+Imports MWBot.net.WikiBot
+
+Public Class GlobalVars
+    Public Shared BotCodename As String = "MWBot.net"
+    Public Shared Exepath As String = AppDomain.CurrentDomain.BaseDirectory
+    Public Shared DirSeparator As String = IO.Path.DirectorySeparatorChar
+    ''' <summary>
+    ''' El separador de decimales varia segun SO y configuracion regional, eso puede afectar los calculos.
+    ''' </summary>
+    Public Shared DecimalSeparator As String = String.Format(CType(1.1, String)).Substring(1, 1)
+
+    Public Shared OS As String = My.Computer.Info.OSFullName & " " & My.Computer.Info.OSVersion
+    Public Shared MwBotVersion As String = Reflection.Assembly.GetExecutingAssembly.GetName.Version.ToString
+    Public Shared User_Filepath As String = Exepath & "Users.psv"
+    Public Shared Log_Filepath As String = Exepath & "Log.psv"
+    Public Shared ConfigFilePath As String = Exepath & "Config.cfg"
+    Public Shared SettingsPath As String = Exepath & "Settings.psv"
+    Public Shared MaxRetry As Integer = 3
+    Public Shared Uptime As DateTime
+
+End Class
